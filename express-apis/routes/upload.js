1a078c7ddc73ed58b726e46864699cc6e9268b5c
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-11-05 00:51:52 +0900

    modify(develop): upload path

--- express-apis/routes/upload.js
+++ express-apis/routes/upload.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var fs = require('fs');
 // multerのインポート
 var multer = require('multer');
-// 保存先を相対ルートの./uplods/以下へ指定
-var upload = multer({ dest: './uploads/' });
+// 保存先を./uplods/以下へ指定
+var uploads = multer({ dest: './local/uploads/' });
 
 /* /uploadへのgetリクエスト時にアップロード画面を表示 */
 router.get('/', function(req, res, next) {
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 });
 
 /* /uploadへのpostリクエスト時にアップロードを行い、画面を表示 */
-router.post('/', upload.fields([{ name: 'uploadFile' }]), function(
+router.post('/', uploads.fields([{ name: 'uploadFile' }]), function(
   req,
   res,
   next
@@ -20,7 +20,7 @@ router.post('/', upload.fields([{ name: 'uploadFile' }]), function(
   var path = req.files.uploadFile[0].path;
   var filename = req.files.uploadFile[0].filename;
   var originalname = req.files.uploadFile[0].originalname;
-  var targetPath = './uploads/' + originalname;
+  var targetPath = './local/uploads/' + originalname;
 
   console.log(path, filename, originalname);
 
@@ -45,6 +45,43 @@ router.post('/', upload.fields([{ name: 'uploadFile' }]), function(
   res.render('upload', {
     title: 'Upload',
     message: 'アップロードが完了しました'
+  });
+});
+/* /uploadへのpostリクエスト時にアップロードを行い、画面を表示 */
+router.post('/multi', uploads.fields([{ name: 'uploadMultiFile' }]), function(
+  req,
+  res,
+  next
+) {
+  var path = req.files.uploadMultiFile[0].path;
+  var filename = req.files.uploadMultiFile[0].filename;
+  var originalname = req.files.uploadMultiFile[0].originalname;
+  var targetPath = './local/uploads/' + originalname;
+
+  console.log(path, filename, originalname);
+
+  fs.rename(path, targetPath, function(err) {
+    if (err) {
+      throw err;
+    }
+    fs.unlink(path, function() {
+      if (err) {
+        throw err;
+      }
+      res.send(
+        'File uploaded to: ' +
+          targetPath +
+          ' - ' +
+          req.files.thumbnail[0].size +
+          ' bytes'
+      );
+    });
+  });
+
+  res.render('upload', {
+    title: 'Upload',
+    message: 'アップロードが完了しました',
+    messageMulti: '複数ファイルのアップロードが完了しました'
   });
 });
 
